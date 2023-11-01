28c55ef983f5c80712e65766703bfd2b6d4fc624
Yousef Alzeer <yousef.alzeer01@gmail.com>
2023-11-01 13:27:33 -0400

    controllers

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -0,0 +1,37 @@
+package com.techelevator.tenmo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private final AccountDao accountDao;
+
+    @Autowired
+    public AccountController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @GetMapping("/{userId}/balance")
+    public ResponseEntity<BigDecimal> getBalance(@PathVariable int userId) {
+        BigDecimal balance = accountDao.getBalance(userId);
+        if (balance != null) {
+            return new ResponseEntity<>(balance, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    public boolean sendTransfer(TransferDetails transferDetails) {
+        return accountDao.sendTransfer(transferDetails);
+    }
+
+    public List<Transfer> getTransfersByUserId(int userId) {
+        return accountDao.getTransfersByUserId(userId);
+    }
+}
