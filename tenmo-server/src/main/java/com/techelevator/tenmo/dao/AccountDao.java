23b4f8bee03dc01c239dbd743abef1b57956ef39
Bryson Poole <notbryson25@gmail.com>
2023-11-02 09:53:24 -0400

    JbdcAccountDao getUsers()

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,10 +1,12 @@
 package com.techelevator.tenmo.dao;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface AccountDao {
 
 
+    List<String> getUsers();
     BigDecimal getBalance(int accountId);
     BigDecimal addToBalance(BigDecimal amountToAdd, int id);
     BigDecimal subtractFromBalance(BigDecimal amountToSubtract, int id);
